04e96a7f2e80718783dd9697d56465a987446756
Maulik <maulikdholariya14@gmail.com>
2020-08-29 09:03:09 +0530

    .

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -19,7 +19,7 @@ class Photo extends Db_object {
         UPLOAD_ERR_OK => "There is no error",
         UPLOAD_ERR_INI_SIZE => "uploaded file size exceed max file size",
         UPLOAD_ERR_FORM_SIZE => "uploaded file exceeds max file size directive",
-        UPLOAD_ERR_PARTIAL => "only partially uploiaded",
+        UPLOAD_ERR_PARTIAL => "only partially uploaded",
         UPLOAD_ERR_NO_FILE => "no file was uploaded",
         UPLOAD_ERR_NO_TMP_DIR => "missing a temp folder",
         UPLOAD_ERR_CANT_WRITE => "failed to write file to disk",
