7e5dbd0fe6ef8781440d555108a28eea321ab8a5
Maulik <maulikdholariya14@gmail.com>
2020-08-29 13:50:06 +0530

    create edit page

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,17 +2,18 @@
 <?php if (!$session->is_signed_in()) {redirect("login.php");}?>
 <?php
 
-if (empty($_GET['photo_id'])) {
+if (empty($_GET['_id'])) {
 
     redirect(ADMIN_ROOT."photos.php");
 
 }
 
-$photo = Photo::find_by_id($_GET['photo_id']);
+$photo = Photo::find_by_id($_GET['id']);
 
 if ($photo) {
 
     $photo->delete_photo();
+    redirect(ADMIN_ROOT."photos.php");
 
 } else {
 
